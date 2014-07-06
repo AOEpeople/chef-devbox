56186704681d6a3b5b7bdc3346fc753e19294304
Fabrizio Branca <github@fabrizio-branca.de>
2014-07-05 20:30:12 -0700

    Update default.rb

--- site-cookbooks/devbox/attributes/default.rb
+++ site-cookbooks/devbox/attributes/default.rb
@@ -1,6 +1,8 @@
 include_attribute 'apache2'
 default['apache']['default_site_enabled'] = false
 default['apache']['version'] = '2.4'
+normal['apache']['version'] = '2.4'
+override['apache']['version'] = '2.4'
 
 include_attribute 'mysql'
 default['mysql']['server_debian_password'] = 'root'
