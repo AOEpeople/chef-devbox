6f97d39a8531eecd60d611255e7b9ddfe4f8592a
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-02-18 21:53:21 -0800

    Calling provision.sh and added some content

--- setup.sh
+++ setup.sh
@@ -32,10 +32,26 @@ echo
 if [ -f /etc/chef-devbox/Berksfile.lock ] ; then rm /etc/chef-devbox/Berksfile.lock; fi
 if [ -d ~/.berkshelf ] ; then rm -rf ~/.berkshelf; fi
 if [ -d /etc/chef-devbox/cookbooks ] ; then rm -rf /etc/chef-devbox/cookbooks; fi
-cd /etc/chef-devbox && berks vendor /etc/chef-devbox/cookbooks || { echo >&2 "Installing berkshelf depenencies failed"; exit 1; }
+cd /etc/chef-devbox && berks vendor /etc/chef-devbox/cookbooks || { echo >&2 "Installing berkshelf dependencies failed"; exit 1; }
 
-#echo
-#echo "Running Chef..."
-#echo "---------------"
-#echo
-#cd /etc/chef-devbox && chef-solo -c solo.rb -j solo.json || { echo >&2 "Chef provsioning failed"; exit 1; }
+echo
+echo "Running Chef..."
+echo "---------------"
+echo
+cd /etc/chef-devbox/provision.sh || { echo >&2 "Chef provisioning failed"; exit 1; }
+
+echo ""
+echo "What's next?"
+echo "------------"
+echo ""
+echo "(0. Log in to this machine if you're not already here: 'vagrant ssh' or use Putty to connect to 127.0.0.1, port 2222)"
+echo "1. Go to /etc/chef-devbox/data_bags/magento-sites/ and create one or more project json files. Look at 'demo_devbox.json.example' for an example"
+echo "2. Run provisioning again:"
+echo "    sudo /etc/chef-devbox/provision.sh"
+echo "3. Copy the contents of /var/www/hosts.hosts and paste it into you host system's host file (Windows: 'C:\\Windows\\System32\\drivers\\etc\\hosts')"
+echo "4. If you want to install and configure Samba run this command:"
+echo "    sudo cd /etc/chef-devbox && sudo chef-solo -c solo.rb -j solo.samba.json"
+echo "5. Follow the instructions on the login screen (log out and log in again) for more information on how to install/update a build package"
+echo ""
+echo "Have a great day!"
+echo ""
